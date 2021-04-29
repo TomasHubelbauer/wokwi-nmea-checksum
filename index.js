07b0c5a6b09670ac46e8e22d0ff3a04500a5766f
Tomas Hubelbauer <tomas@hubelbauer.net>
2021-04-29 14:04:38 +0200

    Use Node to download and split off helper functions

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
-import { avrInstruction, CPU, AVRUSART, usart0Config } from "avr8js";
-import fetch from 'node-fetch';
+import avr8js from 'avr8js';
 import fs from 'fs';
+import parse from './parse.js';
+import download from './download.js';
 
-void async function () {
-  const sketch = await fs.promises.readFile('nmea-checksum.ino', 'utf-8');
-  const req = await fetch('https://hexi.wokwi.com/build', {
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({ sketch }),
-    method: 'POST',
-  });
-  const { hex, stdout, stderr } = await req.json();
-  console.log('compiler output', stderr || stdout);
-  const program = new Uint16Array(0x8000);
-  loadHex(hex, new Uint8Array(program.buffer));
-  const cpu = new CPU(program);
-  const usart = new AVRUSART(cpu, usart0Config, 16e6);
-  usart.onByteTransmit = (value) => {
-    console.log(value, String.fromCharCode(value));
-  };
-
-  for (; ;) {
-    avrInstruction(cpu);
-    cpu.tick();
-    if (cpu.cycles % 500000 === 0) {
-      await new Promise(resolve => setTimeout(resolve, 0));
-    }
-  }
-}()
-
-function loadHex(source, target) {
-  for (const line of source.split('\n')) {
-    if (line[0] === ':' && line.substr(7, 2) === '00') {
-      const bytes = parseInt(line.substr(1, 2), 16);
-      const addr = parseInt(line.substr(3, 4), 16);
-      for (let i = 0; i < bytes; i++) {
-        target[addr + i] = parseInt(line.substr(9 + i * 2, 2), 16);
-      }
-    }
+try {
+  await fs.promises.unlink('nmea-checksum.uart');
+}
+catch (error) {
+  if (error.code !== 'ENOENT') {
+    throw error;
   }
 }
+
+const program = new Uint16Array(0x8000);
+const cpu = new avr8js.CPU(program);
+
+try {
+  const hex = await fs.promises.readFile('nmea-checksum.hex', 'utf-8');
+  parse(hex, new Uint8Array(program.buffer));
+  console.log('Reused cached HEX');
+}
+catch (error) {
+  const sketch = await fs.promises.readFile('nmea-checksum.ino', 'utf-8');
+  const { hex, stdout, stderr } = await download('https://hexi.wokwi.com/build', { sketch });
+  await fs.promises.writeFile('nmea-checksum.hex', hex);
+  parse(hex, new Uint8Array(program.buffer));
+  console.log('Compiled sketch to HEX');
+  console.log(stdout);
+  console.log(stderr);
+}
+
+const buffer = [];
+const usart = new avr8js.AVRUSART(cpu, avr8js.usart0Config, 16e6);
+usart.onByteTransmit = (value) => buffer.push(value);
+
+// Note that the exact amount of instructions is just shy of 1M and known prior
+let instruction = 0;
+while (++instruction < 1_000_000) {
+  avr8js.avrInstruction(cpu);
+  cpu.tick();
+}
+
+const string = String.fromCharCode(...buffer);
+console.log(string);
+await fs.promises.writeFile('nmea-checksum.uart', string);
